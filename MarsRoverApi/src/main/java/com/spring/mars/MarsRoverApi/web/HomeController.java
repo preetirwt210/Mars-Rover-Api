f261a9306122a87c0879b95ca5f44bf934b36e96
Preeti Rawat <preetirawat711@gmail.com>
2021-03-02 16:49:37 +0530

    adding form

--- MarsRoverApi/src/main/java/com/spring/mars/MarsRoverApi/web/HomeController.java
+++ MarsRoverApi/src/main/java/com/spring/mars/MarsRoverApi/web/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.mars.MarsRoverApi.response.MarsRoverApiResponse;
 import com.spring.mars.MarsRoverApi.service.MarsRoverApiService;
@@ -18,6 +20,13 @@ public class HomeController {
 	public String getHomeView(ModelMap model) {
 	
 		MarsRoverApiResponse roverData= roverService.getRoverData("opportunity");
+		model.put("roverData", roverData);
+		return "index";
+	}
+	
+	@PostMapping("/")
+	public String postHomeView(ModelMap model, @RequestParam String marsApiRoverData) {
+		MarsRoverApiResponse roverData= roverService.getRoverData(marsApiRoverData);
 		model.put("roverData", roverData);
 		return "index";
 	}
